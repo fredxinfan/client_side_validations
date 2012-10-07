71babc17834189411de8bcc3c79d722f5e076616
Xin Fan <fx83060856@gmail.com>
2012-10-07 16:53:34 +1030

    update

---
+++ test/middleware/cases/helper.rb
@@ -0,0 +1,7 @@
+require 'base_helper'
+require 'action_controller'
+require 'action_controller/railtie'
+require 'client_side_validations/middleware'
+require 'client_side_validations/engine'
+
+TestApp::Application.initialize!
